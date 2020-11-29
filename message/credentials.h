75edbfa2b51cd99d0e3e5b216143bf0c3656ed74
emanbrown2020 <45438544+emanbrown2020@users.noreply.github.com>
2020-11-28 23:27:57 -0500

    Update credentials.h

--- message/credentials.h
+++ message/credentials.h
@@ -1,3 +1,3 @@
 const char* _ssid = "Browntown";
 const char* _password = "Alexis139!";
-const String _url = "https://gist.github.com/emanbrown2020/976acbfbe97ad854ce8a9bfd09aed81d";
+const String _url = "emanbrown2020/976acbfbe97ad854ce8a9bfd09aed81d/raw/gistfile1.txt";
